db9c5ee9b616c12b2e085c00fc3d74bc1b2c76e0
Hamza Alayed <me@hamzaalayed.com>
2014-05-10 14:33:14 +0300

    Add New tables Skills and types

--- application/views/Resume/home.php
+++ application/views/Resume/home.php
@@ -106,8 +106,13 @@
             </a>
         </div>
         <div class="section_header resume_section_header">
-            <h2 class="section_title resume_section_title"><a href="#"><span class="icon icon-align-left"></span><span
-                        class="section_name">Resume</span></a><span class="section_icon"></span></h2>
+            <h2 class="section_title resume_section_title">
+                <a href="#">
+                    <span class="icon icon-align-left"></span>
+                    <span class="section_name">Resume</span>
+                </a>
+                <span class="section_icon"></span>
+            </h2>
         </div>
         <div class="section_body resume_section_body">
             <div class="sidebar resume_sidebar">
@@ -115,20 +120,45 @@
                     <h3 class="widget_title">Programming skills</h3>
 
                     <div class="widget_inner style_1">
-                        <div class="skills_row odd first"><span class="caption">Wordpress</span><span
-                                class="progressbar"><span data-process="90%" class="progress blue"><span
-                                        class="value">90%</span></span></span></div>
-                        <div class="skills_row even"><span class="caption">PHP</span><span class="progressbar"><span
-                                    data-process="80%" class="progress aqua"><span class="value">80%</span></span></span>
+                        <div class="skills_row odd first">
+                            <span class="caption">Wordpress</span>
+                            <span class="progressbar">
+                                <span data-process="90%" class="progress blue">
+                                    <span class="value">90%</span>
+                                </span>
+                            </span>
                         </div>
-                        <div class="skills_row odd"><span class="caption">HTML</span><span class="progressbar"><span
-                                    data-process="93%" class="progress green"><span class="value">93%</span></span></span>
+                        <div class="skills_row even">
+                            <span class="caption">PHP</span>
+                            <span class="progressbar">
+                                <span data-process="80%" class="progress aqua">
+                                    <span class="value">80%</span>
+                                </span>
+                            </span>
                         </div>
-                        <div class="skills_row even"><span class="caption">CSS</span><span class="progressbar"><span
-                                    data-process="80%" class="progress yellow"><span class="value">80%</span></span></span>
+                        <div class="skills_row odd">
+                            <span class="caption">HTML</span>
+                            <span class="progressbar">
+                                <span data-process="93%" class="progress green">
+                                    <span class="value">93%</span>
+                                </span>
+                            </span>
                         </div>
-                        <div class="skills_row odd"><span class="caption">MySQL</span><span class="progressbar"><span
-                                    data-process="70%" class="progress red"><span class="value">70%</span></span></span>
+                        <div class="skills_row even">
+                            <span class="caption">CSS</span>
+                            <span class="progressbar">
+                                <span data-process="80%" class="progress yellow">
+                                    <span class="value">80%</span>
+                                </span>
+                            </span>
+                        </div>
+                        <div class="skills_row odd">
+                            <span class="caption">MySQL</span>
+                            <span class="progressbar">
+                                <span data-process="70%" class="progress red">
+                                    <span class="value">70%</span>
+                                </span>
+                            </span>
                         </div>
                     </div>
                 </aside>
@@ -136,15 +166,24 @@
                     <h3 class="widget_title">Graphic Skills</h3>
 
                     <div class="widget_inner style_2">
-                        <div class="skills_row odd"><span class="legend blue"></span><span
-                                class="caption">Adobe Photoshop</span></div>
-                        <div class="skills_row even"><span class="legend aqua"></span><span class="caption">Adobe Illustrator</span>
+                        <div class="skills_row odd">
+                            <span class="legend blue"></span>
+                            <span class="caption">Adobe Photoshop</span>
                         </div>
-                        <div class="skills_row odd"><span class="legend green"></span><span
-                                class="caption">Adobe Indesign</span></div>
-                        <div class="skills_row even"><span class="legend yellow"></span><span
-                                class="caption">Corel Draw</span></div>
-                        <div class="skills_row odd"><span class="legend red"></span><span class="caption">3D Max</span>
+                        <div class="skills_row even">
+                            <span class="legend aqua"></span>
+                            <span class="caption">Adobe Illustrator</span>
+                        </div>
+                        <div class="skills_row odd">
+                            <span class="legend green"></span>
+                            <span  class="caption">Adobe Indesign</span>
+                        </div>
+                        <div class="skills_row even">
+                            <span class="legend yellow"></span>
+                            <span class="caption">Corel Draw</span></div>
+                        <div class="skills_row odd">
+                            <span class="legend red"></span>
+                            <span class="caption">3D Max</span>
                         </div>
                         <div class="svg">
                             <svg xmlns="http://www.w3.org/2000/svg" class="piechart">
@@ -162,33 +201,57 @@
                     <h3 class="widget_title">Language skills</h3>
 
                     <div class="widget_inner style_3">
-                        <div class="skills_row odd first"><span class="caption">English</span><span
-                                class="progressbar"><span data-process="80%" class="progress aqua"></span></span></div>
-                        <div class="skills_row even"><span class="caption">French</span><span class="progressbar"><span
-                                    data-process="60%" class="progress green"></span></span></div>
-                        <div class="skills_row odd"><span class="caption">Spain</span><span class="progressbar"><span
-                                    data-process="80%" class="progress yellow"></span></span></div>
+                        <div class="skills_row odd first">
+                            <span class="caption">English</span>
+                            <span class="progressbar">
+                                <span data-process="80%" class="progress aqua"></span>
+                            </span>
+                        </div>
+                        <div class="skills_row even">
+                            <span class="caption">French</span>
+                            <span class="progressbar">
+                                <span data-process="60%" class="progress green"></span>
+                            </span>
+                        </div>
+                        <div class="skills_row odd">
+                            <span class="caption">Spain</span>
+                            <span class="progressbar">
+                                <span data-process="80%" class="progress yellow"></span>
+                            </span>
+                        </div>
                     </div>
                 </aside>
             </div>
             <div class="wrapper resume_wrapper">
                 <div class="category resume_category resume_category_1 first even">
                     <div class="category_header resume_category_header">
-                        <h3 class="category_title"><span class="category_title_icon aqua"></span>Employment</h3>
+                        <h3 class="category_title">
+                            <span class="category_title_icon aqua"></span>
+                            Employment
+                        </h3>
                     </div>
                     <div class="category_body resume_category_body">
                         <article class="post resume_post resume_post_1 first even">
                             <div class="post_header resume_post_header">
-                                <div class="resume_period"><span class="period_from">2003</span> - <span class="period_to">2007</span>
+                                <div class="resume_period">
+                                    <span class="period_from">2003</span>
+                                    -
+                                    <span class="period_to">2007</span>
                                 </div>
-                                <h4 class="post_title"><span class="post_title_icon aqua"></span><a href="post-text.html">There
-                                        are many variations of passages</a></h4>
+                                <h4 class="post_title">
+                                    <span class="post_title_icon aqua"></span>
+                                    <a href="post-text.html">
+                                        There are many variations of passages
+                                    </a>
+                                </h4>
                                 <h5 class="post_subtitle">Director of department</h5>
                             </div>
                             <div class="post_body resume_post_body">
-                                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                                <p>
+                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
                                     anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                                    eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
                             </div>
                         </article>
                         <article class="post resume_post resume_post_2 odd">
--- db.sql
+++ db.sql
@@ -1,22 +1,20 @@
 -- phpMyAdmin SQL Dump
 -- version 2.8.0.1
 -- http://www.phpmyadmin.net
---
+-- 
 -- Host: custsql-ipg42.eigbox.net
--- Generation Time: May 08, 2014 at 02:54 PM
+-- Generation Time: May 09, 2014 at 06:26 AM
 -- Server version: 5.5.32
 -- PHP Version: 4.4.9
---
+-- 
 -- Database: `myresume`
---
-CREATE DATABASE `myresume` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `myresume`;
+-- 
 
 -- --------------------------------------------------------
 
---
+-- 
 -- Table structure for table `profile`
---
+-- 
 
 CREATE TABLE `profile` (
   `profileId` int(9) NOT NULL AUTO_INCREMENT COMMENT 'Profile ID',
@@ -30,9 +28,45 @@ CREATE TABLE `profile` (
   `profileAbout` text NOT NULL COMMENT 'User About',
   `profileImg` text NOT NULL COMMENT 'User Image',
   PRIMARY KEY (`profileId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='User Profile Information' AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='User Profile Information' AUTO_INCREMENT=2 ;
 
---
+-- 
 -- Dumping data for table `profile`
---
+-- 
+
+INSERT INTO `profile` VALUES (1, 'Hamza Alayed', '1985-09-07', 'Arjan, Amman, Jordan', '+962785330496', 'me@hamzaalayed.com', 'http://www.hamzaalayed.com/', 'WEB / MOBILE DEVELOPER  Projects Analyst', 'I taught myself how to learn quickly. \r\nThis way gave me a lot of skills in a short time. \r\nI started in 2005 to now I''ve gained a lot of skills and I’m looking to earn more.\r\nMy objective is Obtain a position where I can maximize my Development skills', 'http://hamzaalayed.com/images/pic.png');
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `skillTypes`
+-- 
+
+CREATE TABLE `skillTypes` (
+  `typeId` int(5) NOT NULL AUTO_INCREMENT COMMENT 'Type ID',
+  `typeName` varchar(150) NOT NULL COMMENT 'Type Name',
+  PRIMARY KEY (`typeId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Skills Type' AUTO_INCREMENT=1 ;
+
+-- 
+-- Dumping data for table `skillTypes`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `skilles`
+-- 
+
+CREATE TABLE `skilles` (
+  `skillId` int(5) NOT NULL AUTO_INCREMENT COMMENT 'Skill ID',
+  `skillTitel` varchar(150) NOT NULL COMMENT 'Skill Header',
+  `skillType` int(5) NOT NULL COMMENT 'Skill Type',
+  PRIMARY KEY (`skillId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Skills Info' AUTO_INCREMENT=1 ;
+
+-- 
+-- Dumping data for table `skilles`
+-- 
 
